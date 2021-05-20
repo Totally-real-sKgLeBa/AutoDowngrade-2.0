c8fbe0e7c3914a6697ceb0b0264065c7e35775d6
davidpopovic13+skglebafake@gmail,com <davidpopovic13+skglebafake@gmail.com>
2021-05-20 20:38:00 +0200

    Initial release

---
+++ resources/autovpk.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:72ca413367f4e6238068ec9497c40c553ecb1888fb3f09dc2a5176a6e8e7994b
+size 277
---
+++ script.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f1b189a70dea369a485c21a6f74620540a2d15f800978360285dbe307d62cc00
+size 360
